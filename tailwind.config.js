d30c92a286d354f89d77a601f85e2704f7ee64fc
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2024-05-17 11:09:48 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
 export const theme = {
-  extend: {},
+  extend: {
+    screens: {
+      mb: "480px",
+    },
+  },
 };
 export const plugins = [];
